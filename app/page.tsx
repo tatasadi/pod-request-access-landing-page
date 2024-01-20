ae12be5ab8cd700da7d2f7d5291073fe142cd5ac
Ehsan Tatasadi <ehsan@tatasadi.com>
2024-01-20 18:06:09 +0100

    finish mobile design

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,97 @@
-import Reference from "./components/Reference"
+"use client"
+import Reference from "@/components/Reference"
+import logo from "../public/images/desktop/logo.svg"
+import Image from "next/image"
+import applePodcast from "../public/images/desktop/apple-podcast.svg"
+import googlePodcast from "../public/images/desktop/google-podcasts.svg"
+import pocketCast from "../public/images/desktop/pocket-casts.svg"
+import spotify from "../public/images/desktop/spotify.svg"
+import { Button } from "@/components/ui/button"
+import { z } from "zod"
+import { useState } from "react"
+import { Input } from "@/components/ui/input"
+
+const emailSchema = z.object({
+  email: z
+    .string()
+    .min(1, "Oops! Please add your email")
+    .email("Oops! Please check your email"),
+})
 
 export default function Home() {
+  const [email, setEmail] = useState("")
+  const [error, setError] = useState("")
+  const [success, setSuccess] = useState(false)
+
+  function handleEmailChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setSuccess(false)
+    setEmail(e.target.value)
+    if (error) setError("")
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const result = emailSchema.safeParse({ email })
+    if (!result.success) {
+      setError(result.error.errors[0].message)
+      setSuccess(false)
+      return
+    } else {
+      setSuccess(true)
+      setEmail("")
+    }
+  }
+
   return (
-    <main className="flex h-full min-h-screen w-full max-w-6xl flex-col items-center px-6 pt-[5.5rem] md:min-h-0">
-      <h1>Hello World</h1>
+    <main className="relative flex h-full min-h-screen w-full max-w-6xl flex-col items-center px-6 md:min-h-0">
+      <div className="absolute inset-0 z-[-1] bg-opacity-50 bg-[url('../public/images/mobile/image-host.jpg')] bg-cover bg-no-repeat"></div>
+      <div className="bg-neutral-dark absolute inset-0 z-[-1] bg-opacity-90"></div>
+      <Image src={logo} alt="logo" className="my-12" />
+      <h1 className="text-light mb-4 text-center text-[1.625rem] uppercase leading-[2.375rem]">
+        <span className="block text-primary-green">Publish your podcasts</span>{" "}
+        everywhere.
+      </h1>
+      <p className="text-neutral-gray-100 mb-8 text-center text-[0.9375rem] leading-[1.5625rem]">
+        Upload your audio to Pod with a single click. We&apos;ll then distribute
+        your podcast to Spotify, Apple Podcasts, Google Podcasts, Pocket Casts
+        and more!
+      </p>
+      <div className="mb-12 flex items-center gap-6">
+        <Image src={spotify} alt="spotify" className="w-[3.5rem]" />
+        <Image
+          src={applePodcast}
+          alt="apple podcast"
+          className="w-[2.8125rem]"
+        />
+        <Image
+          src={googlePodcast}
+          alt="google podcast"
+          className="w-[4.5625rem]"
+        />
+        <Image src={pocketCast} alt="pocket cast" className="w-[4.8125rem]" />
+      </div>
+      <form onSubmit={handleSubmit} className="w-full">
+        <Input
+          name="email"
+          placeholder="Email Address"
+          type="text"
+          value={email}
+          onChange={handleEmailChange}
+        />
+        {error && (
+          <p className="ml-8 mt-2 text-xs italic leading-normal text-primary-red">
+            {error}
+          </p>
+        )}
+        <Button type="submit" className="mt-4" variant="default">
+          Request Access
+        </Button>
+        {success && (
+          <p className="ml-6 mt-4 text-center leading-normal text-primary-green md:text-base">
+            Thank you for requesting access!
+          </p>
+        )}
+      </form>
       <Reference />
     </main>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,28 @@
 import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         primary: {
           green: "hsl(157, 74%, 62%)",
+          "light-green": "hsl(157, 100%, 85%)",
+          red: "hsl(0, 96%, 61%)",
         },
         neutral: {
           "gray-700": "hsl(225, 26%, 23%)",
@@ -19,8 +31,23 @@ const config: Config = {
           dark: "hsl(224, 35%, 11%)",
         },
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
-}
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
 export default config
