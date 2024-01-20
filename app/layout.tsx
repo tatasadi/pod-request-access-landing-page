27d1daccd92296d102c63877d2591d0e825dfba6
Ehsan Tatasadi <ehsan@tatasadi.com>
2024-01-20 16:01:41 +0100

    init project

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next"
+import { Poppins } from "next/font/google"
+import "./globals.css"
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+})
+
+export const metadata: Metadata = {
+  title: "Intro component with sign up form",
+  description: "A Challenge from Frontend Mentor!",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="/images/favicon-32x32.png"
+          type="image/png"
+          sizes="32x32"
+        />
+      </head>
+      <body
+        className={`${poppins.className} flex min-h-screen flex-col items-center justify-center bg-primary-red`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Reference from "./components/Reference"
+
+export default function Home() {
+  return (
+    <main className="flex h-full min-h-screen w-full max-w-6xl flex-col items-center px-6 pt-[5.5rem] md:min-h-0">
+      <h1>Hello World</h1>
+      <Reference />
+    </main>
+  )
+}
